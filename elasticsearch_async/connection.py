d76e41c5d3107fc8cdce7c102fb277a925cb0b0d
Nicholas A <nicholas@santos.ee>
2020-05-21 23:22:01 +0300

    Update connection.py

--- elasticsearch_async/connection.py
+++ elasticsearch_async/connection.py
@@ -7,10 +7,10 @@ import aiohttp
 from aiohttp.client_exceptions import ServerFingerprintMismatch
 import async_timeout
 
-from elasticsearch.exceptions import ConnectionError, ConnectionTimeout, ImproperlyConfigured, SSLError
-from elasticsearch.connection import Connection
-from elasticsearch.compat import urlencode
-from elasticsearch.connection.http_urllib3 import create_ssl_context
+from elasticsearch6.exceptions import ConnectionError, ConnectionTimeout, ImproperlyConfigured, SSLError
+from elasticsearch6.connection import Connection
+from elasticsearch6.compat import urlencode
+from elasticsearch6.connection.http_urllib3 import create_ssl_context
 
 
 class AIOHttpConnection(Connection):
